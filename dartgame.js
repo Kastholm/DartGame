2f1550a0b924b62f684b132fb29908a6af579202
Kastholm <Kastholm1995@gmail.com>
2022-09-11 19:18:04 +0200

    cursor update

--- dartgame.js
+++ dartgame.js
@@ -30,8 +30,36 @@ document
     addPlayer.remove();
   });
 
-let points = document
+
+
+
+
+
+
+
+
+
+
+  
+/* let points = document
   .querySelectorAll(".check")
   .addEventListener("click", function addPoint() {
     console.log(points);
-  });
+  }); */
+
+
+const cursorRounded = document.querySelector('.rounded');
+const cursorPointed = document.querySelector('.pointed');
+
+
+const moveCursor = (e)=> {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+   
+  cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  
+  cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+ 
+}
+
+window.addEventListener('mousemove', moveCursor)
