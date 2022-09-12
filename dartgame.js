05c2c49ac1c16d6b146b7f23b90b112a4fb37765
Kastholm <Kastholm1995@gmail.com>
2022-09-12 21:59:03 +0200

    major upgrade

--- dartgame.js
+++ dartgame.js
@@ -1,7 +1,12 @@
+/* const remPlayer = document.querySelectorAll(".rem");
+remPlayer.forEach((box, index) => {
+  if (index === 0) return;
+  box.style.display = "none";
+}); */
+
 // Tilføj spiller
 document.querySelector(".add").addEventListener("click", function addPlayer() {
   console.log("clicked");
-  /* let name = prompt("Hvad er spillerens navn?"); */
   (async () => {
     const { value: text } = await Swal.fire({
       title: "Skriv holdets navn",
@@ -11,11 +16,10 @@ document.querySelector(".add").addEventListener("click", function addPlayer() {
     });
 
     if (text) {
-      /* Swal.fire(`Entered email: ${text}`) */
       const addPlayer = document.createElement("div");
       addPlayer.innerHTML = `<div class="player">
-      <span class="empty"><input value='${text}'></input><i class="rem fa-sharp fa-solid fa-xmark"></i></span>
-      <span class="check checkOne"><input class='checky' id='check' type="checkbox"><input class='checkyy' type="checkbox"><input class='checkyyy' type="checkbox"></input></span>
+      <span class="empty"><input value='${text}'></input></span>
+      <span class="check checkOne"><input  class='tyve' type="checkbox"><input class='tyve' type="checkbox"><input class='tyve' type="checkbox"></input></span>
       <span class="check"><input type="checkbox"><input type="checkbox"><input type="checkbox"></input></span>
       <span class="check"><input type="checkbox"><input type="checkbox"><input type="checkbox"></input></span>
       <span class="check"><input type="checkbox"><input type="checkbox"><input type="checkbox"></input></span>
@@ -28,32 +32,59 @@ document.querySelector(".add").addEventListener("click", function addPlayer() {
       <span class="check"><input type="checkbox"><input type="checkbox"><input type="checkbox"></input></span>
       </div>`;
       document.querySelector(".dart-body").appendChild(addPlayer);
-      boxAdd();
+      document.querySelector(".start").style.display = "block";
+      document.querySelector(".restart").style.marginLeft = ".5em";
+      /*  if(document.querySelectorAll('.tyve')[0].checked & document.querySelectorAll('.tyve')[1].checked && document.querySelectorAll('.tyve')[2].checked){
+        document.querySelector('.tyve').style.fontSize = "5rem";
+      } */
     }
   })();
-
-  /* if (name === "h") {
-  } else {
-    const addPlayer = document.createElement("div");
-    addPlayer.innerHTML = `<div class="player">
-      <span class="empty"><input value='${name}'></input><i class="rem fa-sharp fa-solid fa-xmark"></i></span>
-      <span class="check"><input type="checkbox"><input type="checkbox"><input type="checkbox"></input></span>
-      <span class="check"><input type="checkbox"><input type="checkbox"><input type="checkbox"></input></span>
-      <span class="check"><input type="checkbox"><input type="checkbox"><input type="checkbox"></input></span>
-      <span class="check"><input type="checkbox"><input type="checkbox"><input type="checkbox"></input></span>
-      <span class="check"><input type="checkbox"><input type="checkbox"><input type="checkbox"></input></span>
-      <span class="check"><input type="checkbox"><input type="checkbox"><input type="checkbox"></input></span>
-      <span class="check"><input type="checkbox"><input type="checkbox"><input type="checkbox"></input></span>
-      <span class="check"><input type="checkbox"><input type="checkbox"><input type="checkbox"></input></span>
-      <span class="check"><input type="checkbox"><input type="checkbox"><input type="checkbox"></input></span>
-      <span class="check"><input type="checkbox"><input type="checkbox"><input type="checkbox"></input></span>
-      <span class="check"><input type="checkbox"><input type="checkbox"><input type="checkbox"></input></span>
-      </div>`;
-    document.querySelector(".dart-body").appendChild(addPlayer);
-  } */
 });
 
 //Fjern Spiller
+function remPlayer() {
+  console.log(document.querySelectorAll(".player").length);
+  console.log("space");
+  console.log(document.querySelectorAll(".rem").length);
+  Swal.fire({
+    title: "Vil du slette spiller og progression?",
+    showDenyButton: true,
+    confirmButtonText: "Slet",
+    denyButtonText: `Slet ikke`,
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+      switch (document.querySelectorAll(".player").length) {
+        case 1:
+          document.querySelectorAll(".player")[0].remove();
+          break;
+        case 2:
+          document.querySelectorAll(".player")[1].remove();
+          break;
+        case 3:
+          document.querySelectorAll(".player")[2].remove();
+          break;
+        case 4:
+          document.querySelectorAll(".player")[3].remove();
+          break;
+        case 5:
+          document.querySelectorAll(".player")[4].remove();
+          break;
+        case 6:
+          document.querySelectorAll(".player")[5].remove();
+          break;
+        case 7:
+          document.querySelectorAll(".player")[6].remove();
+          break;
+        case 8:
+          document.querySelectorAll(".player")[7].remove();
+          break;
+      }
+    } else if (result.isDenied) {
+      Swal.fire("Spilleren er ikke slettet", "", "info");
+    }
+  });
+}
 
 //Genstart Spil
 document
@@ -145,3 +176,64 @@ players.forEach((box, index) => {
     }
   });
 } */
+
+let player = 0;
+function StartSpil() {
+  let startGame = true;
+  let playCount = document.querySelectorAll(".player").length;
+  document.querySelector(".start").addEventListener("click", function start() {
+    console.log(startGame);
+    if (startGame === true) {
+      document.querySelectorAll(".player")[0].classList.add('activePlayer');
+      document.querySelector(".start").innerHTML = "Stop Spil";
+      document.querySelector(".add").style.display = "none";
+      document.querySelector(".remove").style.display = "none";
+      document.querySelector(".next").style.display = "block";
+      document.querySelector(".prev").style.display = "block";
+    } else if (startGame === false) {
+      document.querySelector(".start").innerHTML = "Start Spil";
+      document.querySelectorAll(".player")[0].classList.remove('activePlayer');
+      document.querySelector(".add").style.display = "block";
+      document.querySelector(".remove").style.display = "block";
+      document.querySelector(".next").style.display = "none";
+      document.querySelector(".prev").style.display = "none";
+    }
+    startGame = !startGame;
+  });
+
+  // Næste Spiller
+  document.querySelector(".next").addEventListener("click", function next() {
+    console.log(player);
+    console.log(document.querySelectorAll(".player").length);
+
+    switch (player) {
+      case document.querySelectorAll(".player").length:
+        player--;
+        break;
+      default:
+        player++;
+          document.querySelectorAll(".player")[0 + player].classList.add('activePlayer');
+          document.querySelectorAll(".player")[player - 1].classList.remove('activePlayer');
+    }
+  });
+
+// Forrige Spiller
+  document.querySelector(".prev").addEventListener("click", function prev() {
+    console.log(player);
+    console.log(document.querySelectorAll(".player").length);
+
+    switch (player) {
+      case 0:
+        player = 0;
+        break;
+      default:
+        player--;
+          document.querySelectorAll(".player")[player].classList.add('activePlayer');
+          document.querySelectorAll(".player")[player + 1].classList.remove('activePlayer');
+
+      /*     document.querySelectorAll(".player")[player].classList.add('.activePlayer');
+        document.querySelectorAll(".player")[player + 1].classList.add('.activePlayer'); */
+    }
+  });
+}
+StartSpil();
